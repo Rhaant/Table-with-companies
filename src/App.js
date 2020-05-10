0ecfadf43ed4c7da33ac487c57f6e9216d23732d
Leszek <leszek.synowiec@gmail.com>
2020-05-10 20:31:57 +0200

    add home view component and style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.scss';
 
+import Home from './components/views/home.component'
+
 function App() {
   return (
     <div className="App">
+      <Home />
     </div>
   );
 }
---
+++ src/components/views/home.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './home.styles.scss'
+
+const Home = () => (
+    <div className="home">
+        <div className="header"> Header </div>
+        <div className="table"> Some Table </div>
+    </div>
+)
+
+export default Home
