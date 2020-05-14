7c02aaa74246771df8fda45a157f7355a7aa96bb
Leszek <leszek.synowiec@gmail.com>
2020-05-14 16:16:01 +0200

    add next tests to tableHeader

--- src/components/tableHeader/tableHeader.test.js
+++ src/components/tableHeader/tableHeader.test.js
@@ -24,4 +24,51 @@ describe("TableHead component", () => {
     const component = shallow(<TableHead headers={headersTest} />);
     expect(component).toMatchSnapshot();
   });
+  it('function shoud be called', ()=> {
+    const headersTest = [
+      {
+        id: "test Id 1",
+        value: "test Name 1",
+        position: "test Position 1",
+      },
+      {
+        id: "test Id 2",
+        value: "test Name 2",
+        position: "test Position 2",
+      },
+      {
+        id: "test Id 3",
+        value: "test Name 3",
+        position: "test Position 3",
+      },
+    ];
+    const handleSortChange = jest.fn()
+    const component = shallow(<TableHead headers={headersTest} handleSortChange={handleSortChange} />);
+    component.find('button').first().simulate('click')
+    expect(handleSortChange).toHaveBeenCalled()
+  })
+  it('button shoud shoud have expected value', ()=> {
+    const headersTest = [
+      {
+        id: "test Id 1",
+        value: "test Name 1",
+        position: "test Position 1",
+      },
+      {
+        id: "test Id 2",
+        value: "test Name 2",
+        position: "test Position 2",
+      },
+      {
+        id: "test Id 3",
+        value: "test Name 3",
+        position: "test Position 3",
+      },
+    ];
+    const handleSortChange = jest.fn()
+    const component = shallow(<TableHead headers={headersTest} handleSortChange={handleSortChange} />);
+    component.find('button').first().simulate('click')
+    expect(component.find('button').first().prop('value')).toEqual("test Name 1")
+
 });
+})
